d2b5909f2a9df85af53810e6620157cc84246208
Arto Bendiken <arto@bendiken.net>
2016-08-02 16:13:15 +0200

    Improved the HelloNetwork.test_dns function.

--- hello_network/lib/hello_network.ex
+++ hello_network/lib/hello_network.ex
@@ -23,8 +23,8 @@ defmodule HelloNetwork do
     {:ok, _} = SSDPServer.publish usn, st
   end
 
-  @doc "Attempts to perform a DNS lookup of nerves-project.org."
-  def test_dns do
-    :inet_res.lookup('nerves-project.org', :in, :a)
+  @doc "Attempts to perform a DNS lookup to test connectivity."
+  def test_dns(hostname \\ 'nerves-project.org') do
+    :inet_res.gethostbyname(hostname)
   end
 end
