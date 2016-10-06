23ec5b0e4d9e7f6231ccfdcc68eb15b12cc59c48
Guillaume Ballet <gballet@gmail.com>
2016-10-06 08:24:31 +0200

    Use triggers instead of infinite loops for blinky

--- blinky/lib/blinky.ex
+++ blinky/lib/blinky.ex
@@ -16,23 +16,26 @@ defmodule Blinky do
   def start(_type, _args) do
     led_list = Application.get_env(:blinky, :led_list)
     Logger.debug "list of leds to blink is #{inspect led_list}"
-    spawn fn -> blink_list_forever(led_list) end
+    Enum.each(led_list, &start_blink(&1))
     {:ok, self}
   end
 
-  # call blink_led on each led in the list sequence, repeating forever
-  defp blink_list_forever(led_list) do
-    Enum.each(led_list, &blink(&1))
-    blink_list_forever(led_list)
+  # Set led `led_key` to the state defined below. It is also possible
+  # to globally define states in `config/config.exs` by passing a list
+  # of states with the `:states` keyword.
+  #
+  # The first parameter must be an atom.
+  @spec start_blink(Keyword.T) :: true
+  defp start_blink(led_key) do
+    Logger.debug "blinking led #{inspect led_key}"
+    # led_key is a variable that contains an atom
+    Leds.set [{
+      led_key,
+      [
+        trigger: "timer",
+        delay_off: @off_duration,
+        delay_on: @on_duration * 10
+      ]
+    }]
   end
-
-  # given an led key, turn it on for 100ms then back off
-  defp blink(led_key) do
-    #Logger.debug "blinking led #{inspect led_key}"
-    Leds.set [{led_key, true}]
-    :timer.sleep @on_duration
-    Leds.set [{led_key, false}]
-    :timer.sleep @off_duration
-  end
-
 end
