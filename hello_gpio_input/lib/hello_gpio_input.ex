ca2398e0d89ccc910e285cb3570fe70bc5b4bbbc
Greg Mefford <greg@gregmefford.com>
2017-02-15 21:51:50 -0500

    Replace IO.puts with Logger

--- hello_gpio_input/lib/hello_gpio_input.ex
+++ hello_gpio_input/lib/hello_gpio_input.ex
@@ -1,10 +1,12 @@
 defmodule HelloGpioInput do
   use Application
 
+  require Logger
+
   @input_pin Application.get_env(:hello_gpio_input, :inputpin)[:pin]
 
   def start(_type, _args) do
-    IO.puts "Starting input on pin #{@input_pin}"
+    Logger.debug "Starting input on pin #{@input_pin}"
     {:ok, pid} = Gpio.start_link(@input_pin, :input)
 
     # Start listening for interrupts
@@ -16,9 +18,9 @@ defmodule HelloGpioInput do
     # Infinite loop receiving interrupts from gpio
     receive do
       {:gpio_interrupt, p, :rising} ->
-        IO.puts "Received raising event on pin #{p}"
+        Logger.debug "Received rising event on pin #{p}"
       {:gpio_interrupt, p, :falling} ->
-        IO.puts "Received fallig event on pin #{p}"
+        Logger.debug "Received falling event on pin #{p}"
     end
     loop()
   end
