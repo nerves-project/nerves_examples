7da27919c1466fad7eedeaaf596b562eaa180bfa
Connor Rigby <connor@farmbot.io>
2018-03-17 15:48:02 -0700

    Fix neopixle mixfile.

--- neopixel/mix.exs
+++ neopixel/mix.exs
@@ -38,7 +38,7 @@ defmodule NeoPixel.MixProject do
   end
 
   def application(_target) do
-    [mod: {NeoPixel.Application, []}, extra_applications: [:logger]]
+    [mod: {NeoPixel, []}, extra_applications: [:logger]]
   end
 
   # Run "mix help deps" to learn about dependencies.
@@ -61,9 +61,5 @@ defmodule NeoPixel.MixProject do
   defp system("rpi0"), do: [{:nerves_system_rpi0, "~> 1.0-rc", runtime: false}]
   defp system("rpi2"), do: [{:nerves_system_rpi2, "~> 1.0-rc", runtime: false}]
   defp system("rpi3"), do: [{:nerves_system_rpi3, "~> 1.0-rc", runtime: false}]
-  defp system("bbb"), do: [{:nerves_system_bbb, "~> 1.0-rc", runtime: false}]
-  defp system("ev3"), do: [{:nerves_system_ev3, "~> 1.0-rc", runtime: false}]
-  defp system("qemu_arm"), do: [{:nerves_system_qemu_arm, "~> 1.0-rc", runtime: false}]
-  defp system("x86_64"), do: [{:nerves_system_x86_64, "~> 1.0-rc", runtime: false}]
-  defp system(target), do: Mix.raise("Unknown MIX_TARGET: #{target}")
+  defp system(_), do: Mix.raise("This project only supports Raspberry Pi targets.")
 end
