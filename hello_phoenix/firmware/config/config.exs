984a30c34f125403127d7a4223ab2c000e3c8f24
connor rigby <connor@farmbot.io>
2018-04-27 19:52:52 -0700

    Add nerves_init_gadget to hello_phoenix

--- hello_phoenix/firmware/config/config.exs
+++ hello_phoenix/firmware/config/config.exs
@@ -43,8 +43,26 @@ config :ui, Ui.Endpoint,
 # docs for separating out critical OTP applications such as those
 # involved with firmware updates.
 config :shoehorn,
-  init: [:nerves_runtime],
+  init: [:nerves_runtime, :nerves_init_gadget],
   app: Mix.Project.config()[:app]
+
+# Allows over the air updates via SSH.
+config :nerves_firmware_ssh,
+  authorized_keys: [
+    File.read!(Path.join(System.user_home!(), ".ssh/id_rsa.pub"))
+  ]
+
+# Allows for tailing of logs.
+config :logger, backends: [RingLogger]
+
+# Set a mdns domain and node_name to be able to remsh into the device.
+config :nerves_init_gadget,
+  node_name: :hello_phoenix,
+  mdns_domain: "hello_phoenix.local"
+
+# for Devices that don't support usb gadget such as raspberry pi 1, 2, and 3.
+# address_method: :dhcp,
+# ifname: "eth0"
 
 # Import target specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
