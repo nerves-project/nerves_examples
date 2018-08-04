2e7965c95cd7cabbf4c1e5b201107ff48b473146
Paulo Daniel Gonzalez <pdgonzalez872@gmail.com>
2018-08-04 16:23:23 -0500

    Add nerves_init_gadget, add config to make it work. Add network config as well.

--- hello_phoenix/firmware/config/config.exs
+++ hello_phoenix/firmware/config/config.exs
@@ -13,8 +13,26 @@ config :nerves, :firmware, rootfs_overlay: "rootfs_overlay"
 # docs for separating out critical OTP applications such as those
 # involved with firmware updates.
 config :shoehorn,
-  init: [:nerves_runtime],
+  init: [:nerves_runtime, :nerves_init_gadget],
   app: Mix.Project.config()[:app]
+
+config :nerves_init_gadget,
+  node_name: :base_target,
+  mdns_domain: "nerves_base.local",
+  address_method: :dhcp,
+  ifname: "wlan0"
+
+key_mgmt = System.get_env("NERVES_NETWORK_KEY_MGMT") || "WPA-PSK"
+
+config :nerves_network, :default,
+  wlan0: [
+    ssid: System.get_env("NERVES_NETWORK_SSID"),
+    psk: System.get_env("NERVES_NETWORK_PSK"),
+    key_mgmt: String.to_atom(key_mgmt)
+  ],
+  eth0: [
+    ipv4_address_method: :dhcp
+  ]
 
 # Import target specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
