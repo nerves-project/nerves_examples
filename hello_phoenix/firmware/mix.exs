dfc9874468eae00a77dd4369f02d712b4bf2447e
Greg Mefford <greg@gregmefford.com>
2018-08-04 21:24:31 -0400

    Adjust system versions

--- hello_phoenix/firmware/mix.exs
+++ hello_phoenix/firmware/mix.exs
@@ -48,18 +48,18 @@ defmodule Firmware.MixProject do
   defp deps(target) do
     [
       {:nerves_runtime, "~> 0.4"},
-      {:nerves_init_gadget, "~> 0.3"},
+      {:nerves_init_gadget, "~> 0.4"},
       {:ui, path: "../ui"}
     ] ++ system(target)
   end
 
-  defp system("rpi"), do: [{:nerves_system_rpi, "~> 1.0", runtime: false}]
-  defp system("rpi0"), do: [{:nerves_system_rpi0, "1.1.1", runtime: false}]
-  defp system("rpi2"), do: [{:nerves_system_rpi2, "~> 1.0", runtime: false}]
-  defp system("rpi3"), do: [{:nerves_system_rpi3, "1.1.1", runtime: false}]
-  defp system("bbb"), do: [{:nerves_system_bbb, "~> 1.0", runtime: false}]
-  defp system("ev3"), do: [{:nerves_system_ev3, "~> 1.0", runtime: false}]
+  defp system("rpi"), do: [{:nerves_system_rpi, "~> 1.2", runtime: false}]
+  defp system("rpi0"), do: [{:nerves_system_rpi0, "~> 1.2", runtime: false}]
+  defp system("rpi2"), do: [{:nerves_system_rpi2, "~> 1.2", runtime: false}]
+  defp system("rpi3"), do: [{:nerves_system_rpi3, "~> 1.2", runtime: false}]
+  defp system("bbb"), do: [{:nerves_system_bbb, "~> 1.2", runtime: false}]
+  defp system("ev3"), do: [{:nerves_system_ev3, "~> 1.2", runtime: false}]
   defp system("qemu_arm"), do: [{:nerves_system_qemu_arm, "~> 1.0", runtime: false}]
-  defp system("x86_64"), do: [{:nerves_system_x86_64, "~> 1.0", runtime: false}]
+  defp system("x86_64"), do: [{:nerves_system_x86_64, "~> 1.2", runtime: false}]
   defp system(target), do: Mix.raise("Unknown MIX_TARGET: #{target}")
 end
