64b479ff8d91e65ded89c0cb484c3c57d6e2939e
Justin Schneck <jschneck@mac.com>
2018-10-26 11:16:53 -0400

    Update system versions

--- blinky/mix.exs
+++ blinky/mix.exs
@@ -52,12 +52,11 @@ defmodule Blinky.MixProject do
     ] ++ system(target)
   end
 
-  defp system("rpi"), do: [{:nerves_system_rpi, "~> 1.0", runtime: false}]
-  defp system("rpi0"), do: [{:nerves_system_rpi0, "~> 1.0", runtime: false}]
-  defp system("rpi2"), do: [{:nerves_system_rpi2, "~> 1.0", runtime: false}]
-  defp system("rpi3"), do: [{:nerves_system_rpi3, "~> 1.0", runtime: false}]
-  defp system("bbb"), do: [{:nerves_system_bbb, "~> 1.0", runtime: false}]
-  defp system("ev3"), do: [{:nerves_system_ev3, "~> 1.0", runtime: false}]
-  defp system("x86_64"), do: [{:nerves_system_x86_64, "~> 1.0", runtime: false}]
+  defp system("rpi"), do: [{:nerves_system_rpi, "~> 1.5", runtime: false}]
+  defp system("rpi0"), do: [{:nerves_system_rpi0, "~> 1.5", runtime: false}]
+  defp system("rpi2"), do: [{:nerves_system_rpi2, "~> 1.5", runtime: false}]
+  defp system("rpi3"), do: [{:nerves_system_rpi3, "~> 1.5", runtime: false}]
+  defp system("bbb"), do: [{:nerves_system_bbb, "~> 2.0", runtime: false}]
+  defp system("x86_64"), do: [{:nerves_system_x86_64, "~> 1.5", runtime: false}]
   defp system(target), do: Mix.raise("Unknown MIX_TARGET: #{target}")
 end
