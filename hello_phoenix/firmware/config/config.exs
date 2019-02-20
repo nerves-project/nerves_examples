a53ca13b5c1e41e8655c1ca9311eb2cbf885b3b0
Greg Mefford <greg@gregmefford.com>
2019-02-19 22:32:52 -0500

    Update for Elixir 1.8 and Nerves 1.4

--- hello_phoenix/firmware/config/config.exs
+++ hello_phoenix/firmware/config/config.exs
@@ -50,24 +50,38 @@ config :ui, UiWeb.Endpoint,
 # See https://hexdocs.pm/nerves_firmware_ssh/readme.html for more information
 # on configuring nerves_firmware_ssh.
 
-key = Path.join(System.user_home!(), ".ssh/id_rsa.pub")
-unless File.exists?(key), do: Mix.raise("No SSH Keys found. Please generate an ssh key")
+keys =
+  [
+    Path.join([System.user_home!(), ".ssh", "id_rsa.pub"]),
+    Path.join([System.user_home!(), ".ssh", "id_ecdsa.pub"]),
+    Path.join([System.user_home!(), ".ssh", "id_ed25519.pub"])
+  ]
+  |> Enum.filter(&File.exists?/1)
+
+if keys == [],
+  do:
+    Mix.raise("""
+    No SSH public keys found in ~/.ssh. An ssh authorized key is needed to
+    log into the Nerves device and update firmware on it using ssh.
+    See your project's config.exs for this error message.
+    """)
 
 config :nerves_firmware_ssh,
-  authorized_keys: [
-    File.read!(key)
-  ]
+  authorized_keys: Enum.map(keys, &File.read!/1)
 
 # Configure nerves_init_gadget.
 # See https://hexdocs.pm/nerves_init_gadget/readme.html for more information.
+
+# Setting the node_name will enable Erlang Distribution.
+# Only enable this for prod if you understand the risks.
+node_name = if Mix.env() != :prod, do: "hello_phoenix"
 
 config :nerves_init_gadget,
   ifname: "usb0",
   address_method: :dhcpd,
   mdns_domain: "nerves.local",
-  node_name: nil,
-  node_host: :mdns_domain,
-  ssh_console_port: 22
+  node_name: node_name,
+  node_host: :mdns_domain
 
 # For Devices that don't support usb gadget such as Raspberry Pi 1, 2, and 3:
 # config :nerves_init_gadget,
@@ -78,4 +92,4 @@ config :nerves_init_gadget,
 # of this file so it overrides the configuration defined above.
 # Uncomment to use target specific configurations
 
-# import_config "#{Mix.Project.config[:target]}.exs"
+# import_config "#{Mix.target()}.exs"
--- hello_phoenix/firmware/mix.exs
+++ hello_phoenix/firmware/mix.exs
@@ -1,19 +1,16 @@
 defmodule Firmware.MixProject do
   use Mix.Project
 
-  @target System.get_env("MIX_TARGET") || "host"
+  @all_targets [:rpi, :rpi0, :rpi2, :rpi3, :rpi3a, :bbb, :x86_64]
 
   def project do
     [
       app: :firmware,
       version: "0.1.0",
-      elixir: "~> 1.6",
-      target: @target,
-      archives: [nerves_bootstrap: "~> 1.0"],
-      deps_path: "deps/#{@target}",
-      build_path: "_build/#{@target}",
-      lockfile: "mix.lock.#{@target}",
+      elixir: "~> 1.8",
+      archives: [nerves_bootstrap: "~> 1.4"],
       start_permanent: Mix.env() == :prod,
+      build_embedded: true,
       aliases: [loadconfig: [&bootstrap/1]],
       deps: deps()
     ]
@@ -37,28 +34,25 @@ defmodule Firmware.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:nerves, "~> 1.3", runtime: false},
+      # Dependencies for all targets
+      {:nerves, "~> 1.4", runtime: false},
       {:shoehorn, "~> 0.4"},
-      {:ring_logger, "~> 0.4"}
-    ] ++ deps(@target)
+      {:ring_logger, "~> 0.6"},
+      {:toolshed, "~> 0.2"},
+      {:ui, path: "../ui"},
+
+      # Dependencies for all targets except :host
+      {:nerves_runtime, "~> 0.6", targets: @all_targets},
+      {:nerves_init_gadget, "~> 0.4", targets: @all_targets},
+
+      # Dependencies for specific targets
+      {:nerves_system_rpi, "~> 1.6", runtime: false, targets: :rpi},
+      {:nerves_system_rpi0, "~> 1.6", runtime: false, targets: :rpi0},
+      {:nerves_system_rpi2, "~> 1.6", runtime: false, targets: :rpi2},
+      {:nerves_system_rpi3, "~> 1.6", runtime: false, targets: :rpi3},
+      {:nerves_system_rpi3a, "~> 1.6", runtime: false, targets: :rpi3a},
+      {:nerves_system_bbb, "~> 2.0", runtime: false, targets: :bbb},
+      {:nerves_system_x86_64, "~> 1.6", runtime: false, targets: :x86_64},
+    ]
   end
-
-  # Specify target specific dependencies
-  defp deps("host"), do: []
-
-  defp deps(target) do
-    [
-      {:nerves_runtime, "~> 0.6"},
-      {:nerves_init_gadget, "~> 0.5"},
-      {:ui, path: "../ui"}
-    ] ++ system(target)
-  end
-
-  defp system("rpi"), do: [{:nerves_system_rpi, "~> 1.5", runtime: false}]
-  defp system("rpi0"), do: [{:nerves_system_rpi0, "~> 1.5", runtime: false}]
-  defp system("rpi2"), do: [{:nerves_system_rpi2, "~> 1.5", runtime: false}]
-  defp system("rpi3"), do: [{:nerves_system_rpi3, "~> 1.5", runtime: false}]
-  defp system("bbb"), do: [{:nerves_system_bbb, "~> 2.0", runtime: false}]
-  defp system("x86_64"), do: [{:nerves_system_x86_64, "~> 1.5", runtime: false}]
-  defp system(target), do: Mix.raise("Unknown MIX_TARGET: #{target}")
 end
