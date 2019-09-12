140f4003152dd264b64e3d19b0b48a1f2e797e2d
Frank Hunleth <fhunleth@troodon-software.com>
2019-09-12 09:34:27 -0400

    Build all examples for the rpi4

--- hello_erlang/mix.exs
+++ hello_erlang/mix.exs
@@ -3,7 +3,7 @@ defmodule HelloErlang.MixProject do
 
   @app :hello_erlang
   @version "0.1.0"
-  @all_targets [:rpi, :rpi0, :rpi2, :rpi3, :rpi3a, :bbb, :x86_64]
+  @all_targets [:rpi, :rpi0, :rpi2, :rpi3, :rpi3a, :rpi4, :bbb, :x86_64]
 
   def project do
     [
@@ -53,6 +53,7 @@ defmodule HelloErlang.MixProject do
       {:nerves_system_rpi2, "~> 1.8", runtime: false, targets: :rpi2},
       {:nerves_system_rpi3, "~> 1.8", runtime: false, targets: :rpi3},
       {:nerves_system_rpi3a, "~> 1.8", runtime: false, targets: :rpi3a},
+      {:nerves_system_rpi4, "~> 1.8", runtime: false, targets: :rpi4},
       {:nerves_system_bbb, "~> 2.3", runtime: false, targets: :bbb},
       {:nerves_system_x86_64, "~> 1.8", runtime: false, targets: :x86_64},
     ]
