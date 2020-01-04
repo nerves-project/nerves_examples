d129ba8cbbb8d19af15b0cdd424d56c2a55fe69a
Frank Hunleth <fhunleth@troodon-software.com>
2020-01-04 17:44:39 -0500

    Add minimal example
    The purpose of this is to show a stripped down Nerves application and to
    gather stats on base systems + minimal Erlang/Elixir.

---
+++ minimal/config/target.exs
@@ -0,0 +1,7 @@
+import Config
+  
+# Import target specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+# Uncomment to use target specific configurations
+
+# import_config "#{Mix.target()}.exs"
---
+++ minimal/lib/minimal/application.ex
@@ -0,0 +1,42 @@
+defmodule Minimal.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Minimal.Supervisor]
+    children =
+      [
+        # Children for all targets
+        # Starts a worker by calling: Minimal.Worker.start_link(arg)
+        # {Minimal.Worker, arg},
+      ] ++ children(target())
+
+    Supervisor.start_link(children, opts)
+  end
+
+  # List all child processes to be supervised
+  def children(:host) do
+    [
+      # Children that only run on the host
+      # Starts a worker by calling: Minimal.Worker.start_link(arg)
+      # {Minimal.Worker, arg},
+    ]
+  end
+
+  def children(_target) do
+    [
+      # Children for all targets except host
+      # Starts a worker by calling: Minimal.Worker.start_link(arg)
+      # {Minimal.Worker, arg},
+    ]
+  end
+
+  def target() do
+    Application.get_env(:minimal, :target)
+  end
+end
