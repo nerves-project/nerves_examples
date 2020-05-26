8a7203a643e90a5d440a2a0c609d4a7bbd38a760
Frank Hunleth <fhunleth@troodon-software.com>
2020-05-26 15:31:25 -0400

    Cleanup before bumping deps
    The idea is to remove as much as possible to avoid anything that might
    pollute an update. Erasing all mix.lock files a priori has the benefit
    that if a mix.lock file doesn't get updated that it will show up as
    deleted in git.

--- scripts/update-deps-all.sh
+++ scripts/update-deps-all.sh
@@ -6,9 +6,17 @@ source scripts/projects.sh
 
 update_deps() {
     echo "Updating deps for $1..."
-    (cd $1 && rm -f mix.lock && MIX_TARGET=rpi0 mix deps.update --all)
+    (cd $1 && MIX_TARGET=rpi0 mix deps.update --all)
 }
 
+# Clean everything up
+echo "Cleaning project directory..."
+git clean -fdx
+find . -name deps | xargs rm -fr
+find . -name _build | xargs rm -fr
+find . -name mix.lock -delete
+
+# Now update all deps
 for project in $PROJECTS; do
     update_deps $project
 done
