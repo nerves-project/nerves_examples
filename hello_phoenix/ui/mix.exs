8609037f83bea3ac5fedbc76226c8d35da39eaa0
Connor Rigby <connorr@hey.com>
2020-09-17 14:01:22 -0700

    Update Phoenix Example deps

--- hello_phoenix/ui/mix.exs
+++ hello_phoenix/ui/mix.exs
@@ -34,7 +34,7 @@ defmodule Ui.MixProject do
   defp deps do
     [
       {:phoenix, "~> 1.5.3"},
-      {:phoenix_live_view, "~> 0.13.0"},
+      {:phoenix_live_view, "~> 0.14"},
       {:floki, ">= 0.0.0", only: :test},
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
