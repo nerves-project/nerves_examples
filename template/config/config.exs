bf29d20a2e5e6bd043f796fb62bf7322c0f52b2d
Greg Mefford <greg@gregmefford.com>
2020-10-17 21:46:39 -0400

    Update erlang example

--- template/config/config.exs
+++ template/config/config.exs
@@ -8,7 +8,7 @@ import Config
 # Enable the Nerves integration with Mix
 Application.start(:nerves_bootstrap)
 
-config :hello_erlang, target: Mix.target()
+config :template, target: Mix.target()
 
 # Customize non-Elixir parts of the firmware. See
 # https://hexdocs.pm/nerves/advanced-configuration.html for details.
@@ -18,7 +18,7 @@ config :nerves, :firmware, rootfs_overlay: "rootfs_overlay"
 # Set the SOURCE_DATE_EPOCH date for reproducible builds.
 # See https://reproducible-builds.org/docs/source-date-epoch/ for more information
 
-config :nerves, source_date_epoch: "1577975236"
+config :nerves, source_date_epoch: "1602985334"
 
 # Use Ringlogger as the logger backend and remove :console.
 # See https://hexdocs.pm/ring_logger/readme.html for more information on
