6fb20f788d8097cc7301c683cf1070469bf14a18
Greg Mefford <greg@gregmefford.com>
2020-10-17 22:04:08 -0400

    Add osd32mp1 to project targets

--- scripts/projects.sh
+++ scripts/projects.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TARGETS="rpi0 rpi rpi2 rpi3 rpi3a rpi4 bbb x86_64"
+TARGETS="rpi0 rpi rpi2 rpi3 rpi3a rpi4 bbb x86_64 osd32mp1"
 ELIXIR_PROJECTS="blinky hello_gpio hello_leds minimal hello_phoenix/firmware hello_wifi"
 PROJECTS="$ELIXIR_PROJECTS hello_erlang hello_lfe"
 
