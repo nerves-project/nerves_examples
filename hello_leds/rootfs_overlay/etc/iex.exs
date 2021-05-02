ca3c198a975cae9c733505b83877ae7cf94a2a06
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2021-05-02 10:10:52 -0400

    Add nerves logo to .iex.exs files

--- hello_leds/rootfs_overlay/etc/iex.exs
+++ hello_leds/rootfs_overlay/etc/iex.exs
@@ -1,3 +1,11 @@
+IO.puts("""
+\e[34m████▄▖    \e[36m▐███
+\e[34m█▌  ▀▜█▙▄▖  \e[36m▐█
+\e[34m█▌ \e[36m▐█▄▖\e[34m▝▀█▌ \e[36m▐█   \e[39mN  E  R  V  E  S
+\e[34m█▌   \e[36m▝▀█▙▄▖ ▐█
+\e[34m███▌    \e[36m▀▜████\e[0m
+""")
+
 # Add Toolshed helpers to the IEx session
 use Toolshed
 
