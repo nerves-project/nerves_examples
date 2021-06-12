572ed4f706431cc24e6ebe4924c05f584a99dcb3
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2021-06-11 20:44:30 -0400

    Update hello_phoenix ui deps

--- hello_phoenix/ui/mix.exs
+++ hello_phoenix/ui/mix.exs
@@ -33,12 +33,12 @@ defmodule Ui.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.5.3"},
-      {:phoenix_live_view, "~> 0.14"},
-      {:floki, ">= 0.0.0", only: :test},
+      {:phoenix, "~> 1.5.9"},
+      {:phoenix_live_view, "~> 0.15.1"},
+      {:floki, ">= 0.30.0", only: :test},
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_dashboard, "~> 0.2.0"},
+      {:phoenix_live_dashboard, "~> 0.4"},
       {:telemetry_metrics, "~> 0.4"},
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
