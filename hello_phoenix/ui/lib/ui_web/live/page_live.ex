a34d412138cdff7977c39a380804e6e4ce22b927
Masatoshi N <nishiguchi.masa@gmail.com>
2021-10-09 15:13:30 -0400

    Show firmware serial number on UI

--- hello_phoenix/ui/lib/ui_web/live/page_live.ex
+++ hello_phoenix/ui/lib/ui_web/live/page_live.ex
@@ -5,7 +5,10 @@ defmodule UiWeb.PageLive do
 
   @impl Phoenix.LiveView
   def mount(_params, _session, socket) do
-    socket = assign(socket, current_time_second: System.monotonic_time(:second))
+    socket =
+      socket
+      |> assign(current_time_second: System.monotonic_time(:second))
+      |> assign(serial_number: get_serial_number())
 
     if connected?(socket) do
       schedule_refresh()
@@ -24,5 +27,12 @@ defmodule UiWeb.PageLive do
 
   defp schedule_refresh() do
     Process.send_after(self(), :tick, @refresh_interval_ms)
+  end
+
+  defp get_serial_number() do
+    case Code.ensure_loaded(Nerves.Runtime) do
+      {:module, _} -> Nerves.Runtime.serial_number()
+      _error -> "Unavailable"
+    end
   end
 end
