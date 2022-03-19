af89432aaaa9723f45b9417870023fd1ec6a360e
Jon Carstens <jjcarstens@me.com>
2022-03-19 14:20:18 -0600

    Update Phoenix and deps
    * phoenix 1.6.6
    * phoenix_live_view 0.17.7
    * phoenix_live_dashboard 0.6.5

--- hello_phoenix/ui/mix.exs
+++ hello_phoenix/ui/mix.exs
@@ -39,7 +39,7 @@ defmodule Ui.MixProject do
       {:ecto_sqlite3, ">= 0.0.0"},
       {:phoenix_html, "~> 3.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, "~> 0.16.0"},
+      {:phoenix_live_view, "~> 0.17.0"},
       {:floki, ">= 0.30.0", only: :test},
       {:phoenix_live_dashboard, "~> 0.5"},
       {:esbuild, "~> 0.2", runtime: Mix.env() == :dev && Mix.target() == :host},
