5c29ca8988fb1544a7f11eb0921b2e7661b74ab7
Frank Hunleth <frank@smartrent.com>
2022-10-19 19:26:15 -0400

    Refresh blinky's config files

--- blinky/config/config.exs
+++ blinky/config/config.exs
@@ -1,8 +1,8 @@
 # This file is responsible for configuring your application and its
 # dependencies.
 #
-# This configuration file is loaded before any dependency and
-# is restricted to this project.
+# This configuration file is loaded before any dependency and is restricted to
+# this project.
 import Config
 
 # Enable the Nerves integration with Mix
--- blinky/config/target.exs
+++ blinky/config/target.exs
@@ -1,8 +1,8 @@
 import Config
 
 # Use shoehorn to start the main application. See the shoehorn
-# docs for separating out critical OTP applications such as those
-# involved with firmware updates.
+# library documentation for more control in ordering how OTP
+# applications are started and handling failures.
 
 config :shoehorn, init: [:nerves_runtime, :nerves_pack]
 
@@ -54,31 +54,30 @@ config :vintage_net,
   ]
 
 config :mdns_lite,
-  # The `host` key specifies what hostnames mdns_lite advertises.  `:hostname`
+  # The `hosts` key specifies what hostnames mdns_lite advertises.  `:hostname`
   # advertises the device's hostname.local. For the official Nerves systems, this
-  # is "nerves-<4 digit serial#>.local".  mdns_lite also advertises
-  # "nerves.local" for convenience. If more than one Nerves device is on the
-  # network, delete "nerves" from the list.
+  # is "nerves-<4 digit serial#>.local".  The `"nerves"` host causes mdns_lite
+  # to advertise "nerves.local" for convenience. If more than one Nerves device
+  # is on the network, it is recommended to delete "nerves" from the list
+  # because otherwise any of the devices may respond to nerves.local leading to
+  # unpredictable behavior.
 
-  host: [:hostname, "nerves"],
+  hosts: [:hostname, "nerves"],
   ttl: 120,
 
   # Advertise the following services over mDNS.
   services: [
     %{
-      name: "SSH Remote Login Protocol",
       protocol: "ssh",
       transport: "tcp",
       port: 22
     },
     %{
-      name: "Secure File Transfer Protocol over SSH",
       protocol: "sftp-ssh",
       transport: "tcp",
       port: 22
     },
     %{
-      name: "Erlang Port Mapper Daemon",
       protocol: "epmd",
       transport: "tcp",
       port: 4369
