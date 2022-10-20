3cfcc4f4663ec2a37751ab0dcdd9ca982b35b737
Masatoshi N <nishiguchi.masa@gmail.com>
2022-10-20 08:26:46 -0400

    Remove hello_leds example

--- hello_leds/config/host.exs
+++
@@ -1,4 +0,0 @@
-# Configuration for the host (no LEDs to blink)
-import Config
-
-config :hello_leds, led_list: []
--- hello_leds/config/osd32mp1.exs
+++
@@ -1,5 +0,0 @@
-# Allow project to build, but leave configuration up to the user
-import Config
-
-config :hello_leds, led_list: []
-config :nerves_leds, names: []
