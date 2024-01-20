b340813a6d1a2de3ce68a1e5f43e6294fbb15f32
Jon Carstens <jjcarstens@me.com>
2024-01-20 06:45:32 -0700

    Prevent unsupported `file_system` dep in firmware
    Fixes #386
    
    This is a host-only tool used with `phoenix_live_reload` which can
    cause problems if attempting to run on target. In most cases, this
    dep doesn't pass the checks during firmware compilation since it
    includes host binaries incompatible with the specified target.
    
    This changes that to only compile `:phoenix_live_reload` (and
    subsequently `:file_system`) when the target is `:host`

--- hello_phoenix/ui/mix.exs
+++ hello_phoenix/ui/mix.exs
@@ -38,7 +38,7 @@ defmodule Ui.MixProject do
       {:ecto_sql, "~> 3.6"},
       {:ecto_sqlite3, ">= 0.0.0"},
       {:phoenix_html, "~> 3.0"},
-      {:phoenix_live_reload, "~> 1.2", only: :dev},
+      {:phoenix_live_reload, "~> 1.2", only: :dev, targets: :host},
       {:phoenix_live_view, "~> 0.17.0"},
       {:floki, ">= 0.30.0", only: :test},
       {:phoenix_live_dashboard, "~> 0.5"},
