55e186f3c3bc47e56ae127ad0b43fff2eb957dae
Jon Carstens <jjcarstens@me.com>
2024-01-20 06:54:33 -0700

    Update `hello_live_view` deps

--- hello_live_view/mix.exs
+++ hello_live_view/mix.exs
@@ -54,11 +54,11 @@ defmodule HelloLiveView.MixProject do
     [
       # Initial pheonix deps
       {:phoenix, "~> 1.7.2"},
-      {:phoenix_html, "~> 3.3"},
+      {:phoenix_html, "~> 4.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev, targets: :host},
-      {:phoenix_live_view, "~> 0.18.18"},
+      {:phoenix_live_view, "~> 0.20"},
       {:floki, ">= 0.30.0", only: :test},
-      {:phoenix_live_dashboard, "~> 0.7.2"},
+      {:phoenix_live_dashboard, "~> 0.8"},
       {:esbuild, "~> 0.7", runtime: Mix.env() == :dev},
       {:tailwind, "~> 0.2.0", runtime: Mix.env() == :dev},
       {:telemetry_metrics, "~> 0.6"},
