11e5f5595747b77655a118deee95c6939909c34a
Masatoshi Nishiguchi <7563926+mnishiguchi@users.noreply.github.com>
2024-06-22 08:04:47 +0900

    import heroicons from external source for hello_phoenix

--- hello_phoenix/ui/assets/js/app.js
+++ hello_phoenix/ui/assets/js/app.js
@@ -23,7 +23,10 @@ import {LiveSocket} from "phoenix_live_view"
 import topbar from "../vendor/topbar"
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
+let liveSocket = new LiveSocket("/live", Socket, {
+  longPollFallbackMs: 2500,
+  params: {_csrf_token: csrfToken}
+})
 
 // Show progress bar on live navigation and form submits
 topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
