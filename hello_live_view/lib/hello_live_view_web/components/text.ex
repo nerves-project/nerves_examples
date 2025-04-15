d83ab50095f7bdc8b048f846afc38565c8da2f12
Powell Kinney <628952+pkinney@users.noreply.github.com>
2025-04-15 09:09:44 -0500

    Update LiveView to 1.0.x

--- hello_live_view/lib/hello_live_view_web/components/text.ex
+++ hello_live_view/lib/hello_live_view_web/components/text.ex
@@ -5,7 +5,7 @@ defmodule HelloLiveViewWeb.Components.Text do
     ~H"""
     <header class="bg-white shadow">
       <div class="mx-auto max-w-7xl py-4 px-4 sm:px-4 lg:px-8">
-        <h1 class="text-3xl font-bold tracking-tight text-gray-900"><%= @title %></h1>
+        <h1 class="text-3xl font-bold tracking-tight text-gray-900">{@title}</h1>
       </div>
     </header>
     """
@@ -14,14 +14,14 @@ defmodule HelloLiveViewWeb.Components.Text do
   def body(assigns) do
     ~H"""
     <div class="mx-auto max-w-7xl py-2 sm:px-6 lg:px-8">
-      <%= render_slot(@inner_block) %>
+      {render_slot(@inner_block)}
     </div>
     """
   end
 
   def link_to(assigns) do
     ~H"""
-    <a href={@href} class="text-indigo-600 hover:text-indigo-900"><%= render_slot(@inner_block) %></a>
+    <a href={@href} class="text-indigo-600 hover:text-indigo-900">{render_slot(@inner_block)}</a>
     """
   end
 end
